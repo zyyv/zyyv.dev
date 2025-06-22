53f4afb615331b292ab7a5e2c2ca1610f67d7220
Chris <hizyyv@gmail.com>
2025-06-22 17:14:21 +0800

    chore:update

---
+++ src/components/Quadrant.vue
@@ -0,0 +1,17 @@
+<script lang='ts' setup>
+import { defineProps } from 'vue';
+
+defineProps<{
+  x: number;
+  y: number;
+}>();
+</script>
+
+<template>
+  <div class="w-full h-full grid" :style="{
+    gridTemplateColumns: `${x * 100}% ${100 - x * 100}%`,
+    gridTemplateRows: `${y * 100}% ${100 - origin.y * 100}%`
+  }">
+    <slot />
+  </div>
+</template>
