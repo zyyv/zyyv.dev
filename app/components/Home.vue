963929202cf7a3d993a627985aad1b0896c65dc5
Chris <hizyyv@gmail.com>
2026-04-28 22:22:19 +0800

    feat: update

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -5,14 +5,17 @@ defineProps<{
   posts: PostPreview[]
 }>()
 
-const { center, initQuadrantSizeConfig } = useCenter()
+const {
+  center,
+  getQuadrantScrollProgress,
+  initQuadrantSizeConfig,
+  setQuadrantScrollProgress,
+} = useCenter()
 
 initQuadrantSizeConfig()
 
 onMounted(() => {
   const storage = useLocalStorage('center', center)
-  if (storage.value)
-    center.value = { ...storage.value }
 
   let timeoutId: ReturnType<typeof setTimeout> | null = null
   watchEffect(() => {
@@ -27,17 +30,45 @@ onMounted(() => {
 })
 
 const dragState = ref(false)
+const wheelState = ref(false)
+const scrollProgress = ref(0)
+const scrollSensitivity = 0.0005
+let wheelStateTimer: ReturnType<typeof setTimeout> | null = null
+
+function handleWheel(event: WheelEvent) {
+  event.preventDefault()
+
+  if (dragState.value)
+    return
+
+  wheelState.value = true
+  if (wheelStateTimer)
+    clearTimeout(wheelStateTimer)
+  wheelStateTimer = setTimeout(() => {
+    wheelState.value = false
+  }, 120)
+
+  scrollProgress.value = getQuadrantScrollProgress()
+  scrollProgress.value += event.deltaY * scrollSensitivity
+  setQuadrantScrollProgress(scrollProgress.value)
+}
+
+onBeforeUnmount(() => {
+  if (wheelStateTimer)
+    clearTimeout(wheelStateTimer)
+})
 </script>
 
 <template>
   <section
     class="size-screen grid"
-    :class="{ trans: !dragState }"
+    :class="{ trans: !dragState && !wheelState }"
     :style="{
       gridTemplateColumns: `${center.x * 100}% ${100 - center.x * 100}%`,
       gridTemplateRows: `${center.y * 100}% ${100 - center.y * 100}%`,
-      willChange: dragState ? 'auto' : 'grid-template-columns, grid-template-rows',
+      willChange: dragState || wheelState ? 'auto' : 'grid-template-columns, grid-template-rows',
     }"
+    @wheel="handleWheel"
   >
     <OriginQuadrant quadrant="II">
       <MeInfo />
@@ -53,6 +84,11 @@ const dragState = ref(false)
     </OriginQuadrant>
   </section>
   <div class="absolute inset-0 z-200 pointer-events-none">
-    <OriginController v-model="center" v-model:state="dragState" size-full />
+    <OriginController
+      v-model="center"
+      v-model:state="dragState"
+      :transition-disabled="wheelState"
+      size-full
+    />
   </div>
 </template>
